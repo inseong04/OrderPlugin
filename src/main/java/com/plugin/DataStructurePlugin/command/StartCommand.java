70b2c9168d918dbc72c629ed50ce27ee49ed0ec0
samgashyeong <samgashyeong@naver.com>
2021-10-31 15:36:29 +0900

    set comment and correction model

--- src/main/java/com/plugin/DataStructurePlugin/command/StartCommand.java
+++ src/main/java/com/plugin/DataStructurePlugin/command/StartCommand.java
@@ -1,15 +1,34 @@
 package com.plugin.DataStructurePlugin.command;
 
 import com.plugin.DataStructurePlugin.AppHelper;
+import com.plugin.DataStructurePlugin.DataStructurePlugin;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.Score;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
 
 public class StartCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (AppHelper.isPlayer(sender)) {
-
+            if(args[0] != null){
+                Player p = (Player) sender;
+                p.setCustomNameVisible(true);
+                switch (args[0]){
+                    case "상인" ->{
+                        sender.sendMessage("당신은 상인이 되었습니다.");
+                        p.setCustomName("상인");;
+                    }
+                    case "손님" -> {
+                        sender.sendMessage("당신은 손님이 되었습니다.");
+                        p.setCustomName("손님");
+                    }
+                }
+            }
             return true;
         }
         else {

a8b8b1172d687f37aed8f01a170cde763aed1ab1
samgashyeong <samgashyeong@naver.com>
2021-11-08 10:07:16 +0900

    youtube

--- src/main/java/com/plugin/orderplugin/command/CustomerCommand.java
+++ src/main/java/com/plugin/orderplugin/command/CustomerCommand.java
@@ -9,6 +9,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.ItemMeta;
 
 public class CustomerCommand implements CommandExecutor {
@@ -23,27 +24,35 @@ public class CustomerCommand implements CommandExecutor {
                 }
                 else{
                     merchantName = args[0];
-                    ItemStack BackItem = new ItemStack(Material.RED_BED);
-                    ItemMeta meta = BackItem.getItemMeta();
-                    meta.setDisplayName("뒤로가기");
-                    BackItem.setItemMeta(meta);
+                    switch(merchantName){
+                        case "아이템수령":{
+                            PlayerInventory playerInventory= player.getInventory();
+                            break;
+                        }
+                        default:{
+                            ItemStack BackItem = new ItemStack(Material.RED_BED);
+                            ItemMeta meta = BackItem.getItemMeta();
+                            meta.setDisplayName("뒤로가기");
+                            BackItem.setItemMeta(meta);
 
-                    ItemStack OrderItem = new ItemStack(Material.DARK_OAK_DOOR);
-                    ItemMeta meta1 = BackItem.getItemMeta();
-                    meta1.setDisplayName("주문하기");
-                    OrderItem.setItemMeta(meta1);
+                            ItemStack OrderItem = new ItemStack(Material.DARK_OAK_DOOR);
+                            ItemMeta meta1 = BackItem.getItemMeta();
+                            meta1.setDisplayName("주문하기");
+                            OrderItem.setItemMeta(meta1);
 
-                    ItemStack[] itemStack = {new ItemStack(Material.BREAD), new ItemStack(Material.COOKED_CHICKEN),
-                            new ItemStack(Material.WATER_BUCKET), new ItemStack(Material.MILK_BUCKET),
-                            new ItemStack(Material.TROPICAL_FISH)};
+                            ItemStack[] itemStack = {new ItemStack(Material.BREAD), new ItemStack(Material.COOKED_CHICKEN),
+                                    new ItemStack(Material.WATER_BUCKET), new ItemStack(Material.MILK_BUCKET),
+                                    new ItemStack(Material.TROPICAL_FISH)};
 
-                    Inventory i = Bukkit.createInventory(player, 27, "주문 메뉴");
-                    for(int j=10;j<15;j++){
-                        i.setItem(j+1, itemStack[j-10]);
+                            Inventory i = Bukkit.createInventory(player, 27, "주문 메뉴");
+                            for(int j=10;j<15;j++){
+                                i.setItem(j+1, itemStack[j-10]);
+                            }
+                            i.setItem(18, BackItem);
+                            i.setItem(26, OrderItem);
+                            player.openInventory(i);
+                        }
                     }
-                    i.setItem(18, BackItem);
-                    i.setItem(26, OrderItem);
-                    player.openInventory(i);
                 }
             }
             else{
